fc7ff84fb8ffc578137a85cfe1c35dacd60c9071
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-11-14 16:13:43 -0700

    Exercise two complete.

--- ControlFlowExercisesTwo/Program.cs
+++ ControlFlowExercisesTwo/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a numeric value");
+            var numOne = Console.ReadLine();
+            Console.WriteLine("Now enter in another numeric value");
+            var numTwo = Console.ReadLine();
+            var numOneValue = int.Parse(numOne);
+            var numTwoValue = int.Parse(numTwo);
+
+            if(numOneValue > numTwoValue)
+            {
+                Console.WriteLine(string.Format("Of the two values, {0} is greater than {1}.", numOneValue, numTwoValue));
+            } else if(numOneValue < numTwoValue)
+            {
+                Console.WriteLine(string.Format("Of the two values, {0} is greater than {1}.", numTwoValue, numOneValue));
+            } else if(numOneValue == numTwoValue)
+            {
+                Console.WriteLine("The two values are equal.");
+            }
         }
     }
 }
